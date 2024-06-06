7043e337272a9d2414caa591c5576e7f5dcaa65b
havelessbemore <dev.michael.rojas@gmail.com>
2024-06-05 21:06:03 -0400

    Update examples

--- examples/hamlet/index.js
+++ examples/hamlet/index.js
@@ -1,14 +1,14 @@
 import fs from "node:fs";
+import { createRequire } from "node:module";
 import path from "node:path";
 import readline from "node:readline";
 import url from "node:url";
 
 import { calculateCapacity, Estimator } from "../../dist/index.mjs";
 
-import meta from "./meta.json" with { type: "json" };
-
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const meta = createRequire(import.meta.url)("./meta.json");
 
 async function* getWords(filePath) {
   // Create the input stream
@@ -35,24 +35,21 @@ async function main() {
   const inputPath = path.resolve(__dirname, "input.txt");
 
   // Get estimate; expect 90% accuracy, with 99% confidence
+  let wordCount = 0;
   const capacity = calculateCapacity(meta.total, expectedRelErr, 0.01);
   const cvm = new Estimator(capacity);
   for await (const word of getWords(inputPath)) {
     cvm.add(word);
+    ++wordCount;
   }
   const actual = cvm.estimate();
-  const actualRelErr = actual / meta.unique - 1;
+  const actualRelErr = actual / expected - 1;
 
   // Print results
-  console.log(`Capacity: ${capacity}`);
-  console.table([
-    {
-      Expected: expected,
-      Actual: actual,
-      "Expected Error (%)": `±${(100 * expectedRelErr).toFixed(2)}`,
-      "Actual Error (%)": (100 * actualRelErr).toFixed(2),
-    },
-  ]);
+  console.log(`Total words: ${wordCount}`);
+  console.log(`CVM capacity: ${cvm.capacity}`);
+  console.log(`Expected uniques: ${expected} ± ${(100 * expectedRelErr).toFixed(2)}%`);
+  console.log(`Estimated uniques: ${actual}; ${(100 * actualRelErr).toFixed(2)}% from expected`);
 }
 
 if (process.argv[1] === __filename) {
